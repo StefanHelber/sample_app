afa1d9bba7b04276fc9d1b86cc83e2ae5ae9c9de
Stefan Helber <stefan.helber@prod.uni-hannover.de>
2012-11-05 16:10:06 +0100

    Done with Chapter 10

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -75,7 +75,18 @@ describe "Authentication" do
         end
       end
 
+      describe "in the Microposts controller" do
 
+        describe "submitting to the create action do" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action do" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
     end
 
 
